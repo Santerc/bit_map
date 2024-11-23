714a208219643f1a6687fed185817fdc5c060266
Santerc <597944675@qq.com>
2024-11-23 22:40:42 +0800

    finish search union

--- pages/map/search.js
+++ pages/map/search.js
@@ -1,133 +1,76 @@
 // pages/map/search.js
-//获取应用实例
 var app = getApp();
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
     keyword: null,
-    buildlData: app.globalData.map,
-    showData: null,
+    showData: null,  // 显示的搜索结果
     cursor: 0,
     imgCDN: app.imgCDN
   },
 
-  bindSearchInput: function(e) {
-    let showData = new Array();
-    let searchdata = this.data.buildlData;
-    if (e.detail.cursor >= this.data.cursor) {
-      //输入文字
-      let inputData = e.detail.value.replace(/(^\s*)|(\s*$)/g, "")
+  /**
+   * 输入框变化时触发搜索
+   */
+  bindSearchInput: function (e) {
+    const inputData = e.detail.value.trim();  // 清除前后空白
+    const db = wx.cloud.database();  // 获取云数据库实例
+    const _this = this;
 
-      if (inputData) {
-        let z = 0,
-          x = 100;
-        for (var b in searchdata) {
-          for (var i in searchdata[b].data) {
-            if (searchdata[b].data[i].name.indexOf(inputData) != -1 || (searchdata[b].data[i].floor && searchdata[b].data[i].floor.indexOf(inputData) != -1)) {
-              let build = searchdata[b].data[i];
-              build.tid = b;
-              build.bid = i;
-              z = z + 1;
-              build.index = z;
-              showData.push(build);
-            } else if (searchdata[b].data[i].description.indexOf(inputData) != -1) {
-              let build = searchdata[b].data[i];
-              build.tid = b;
-              build.bid = i;
-              x = x + 1;
-              build.index = x;
-              showData.push(build);
-            }
-          }
-        }
-        //冒泡排序
-        for (var i = 0; i < showData.length; i++) {
-          for (var j = 0; j < showData.length - i - 1; j++) {
-            if (showData[j].index > showData[j + 1].index) {
-              var temp = showData[j];
-              showData[j] = showData[j + 1];
-              showData[j + 1] = temp;
-              console.log('交换' + showData[j].index + ':' + showData[j + 1].index)
-            }
-          }
-        }
-        if (inputData == 'gxgk') {
-          showData.push({
-            name: "\u839e\u9999\u5e7f\u79d1"
+    if (inputData) {
+      // 如果有输入内容，进行云数据库查询
+      db.collection('location')  // 假设数据库集合名为 'locations'
+        .where({
+          // 这里假设你想模糊搜索 'name', 'floor' 和 'description' 字段
+          name: db.RegExp({
+            regexp: inputData,  // 搜索关键字
+            options: 'i'  // 不区分大小写
           })
-        }
-        this.setData({
-          showData: showData
+        })
+        .get({
+          success(res) {
+            console.log('搜索结果：', res.data);
+            _this.setData({
+              showData: res.data  // 将搜索结果显示在页面上
+            });
+          },
+          fail(err) {
+            console.error('查询失败：', err);
+            wx.showToast({
+              title: '搜索失败',
+              icon: 'none'
+            });
+          }
         });
-      }
     } else {
-      //删除文字
-      console.log('删除文字')
+      // 如果输入框为空，清空结果
       this.setData({
         showData: null
       });
     }
-    this.data.cursor = e.detail.cursor;
-  },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function(options) {},
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function() {
-
   },
 
   /**
-   * 生命周期函数--监听页面显示
+   * 页面生命周期函数
    */
-  onShow: function() {
-
-  },
+  onLoad: function (options) {},
+  onReady: function () {},
+  onShow: function () {},
+  onHide: function () {},
+  onUnload: function () {},
+  onPullDownRefresh: function () {},
+  onReachBottom: function () {},
+  onShareAppMessage: function () {},
 
   /**
-   * 生命周期函数--监听页面隐藏
+   * 清空搜索框
    */
-  onHide: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function() {
-
-  },
-  reset: function() {
+  reset: function () {
     this.setData({
-      keyword: null
+      keyword: null,
+      showData: null  // 清空显示的结果
     });
   }
-})
+});
